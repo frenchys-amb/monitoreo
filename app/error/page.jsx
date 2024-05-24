4ed235bf473c5c62d191d0c1a7ba0fc87be20f48
frenchys-amb <frenchysambulance@gmail.com>
2024-05-23 20:44:15 -0400

    Update page.jsx

--- app/error/page.jsx
+++ app/error/page.jsx
@@ -1,3 +1,7 @@
-export default function Error(){
-    return <h1>There was an error.</h1>
+export default function Error() {
+  return (
+    <div className="flex justify-center items-center h-screen bg-gray-800 text-white">
+      <h1 className="text-4xl">There was an error.</h1>
+    </div>
+  );
 }
